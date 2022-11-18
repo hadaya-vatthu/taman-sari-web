d298946560d5b6dd973552755206c91fad41d571
Jordi Yaputra <jordiyaputra@gmail.com>
2022-11-18 16:40:51 +0700

    fix: unable to load env
    Load the env on build time as static env instead.

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,13 +1,4 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit';
 import * as env from '$env/static/public';
 
-if (!env.PUBLIC_SUPABASE_URL || !env.PUBLIC_SUPABASE_ANON_KEY) {
-	console.error({
-		PUBLIC_SUPABASE_URL: env.PUBLIC_SUPABASE_URL,
-		PUBLIC_SUPABASE_ANON_KEY: env.PUBLIC_SUPABASE_ANON_KEY
-	});
-  console.debug(env)
-	throw new Error('Env not loaded!');
-}
-
 export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
