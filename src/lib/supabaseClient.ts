69763b4a201b88f9f8a901b06074721935dc00d3
Jordi Yaputra <jordiyaputra@gmail.com>
2022-11-18 16:18:10 +0700

    logging the env

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,8 +1,13 @@
-import { createClient } from '@supabase/auth-helpers-sveltekit'
-import { env } from '$env/dynamic/public'
+import { createClient } from '@supabase/auth-helpers-sveltekit';
+import { env } from '$env/dynamic/public';
 
 if (!env.PUBLIC_SUPABASE_URL || !env.PUBLIC_SUPABASE_ANON_KEY) {
-  throw new Error('Env not loaded!');
+	console.error({
+		PUBLIC_SUPABASE_URL: env.PUBLIC_SUPABASE_URL,
+		PUBLIC_SUPABASE_ANON_KEY: env.PUBLIC_SUPABASE_ANON_KEY
+	});
+  console.debug(env)
+	throw new Error('Env not loaded!');
 }
 
-export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
+export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY);
