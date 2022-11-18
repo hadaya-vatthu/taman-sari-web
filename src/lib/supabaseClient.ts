a256bc35ee2d891d2b51909789f05c8e808752a0
Jordi Yaputra <jordiyaputra@gmail.com>
2022-11-18 16:15:23 +0700

    add env check

--- src/lib/supabaseClient.ts
+++ src/lib/supabaseClient.ts
@@ -1,4 +1,8 @@
 import { createClient } from '@supabase/auth-helpers-sveltekit'
 import { env } from '$env/dynamic/public'
 
+if (!env.PUBLIC_SUPABASE_URL || !env.PUBLIC_SUPABASE_ANON_KEY) {
+  throw new Error('Env not loaded!');
+}
+
 export const supabaseClient = createClient(env.PUBLIC_SUPABASE_URL, env.PUBLIC_SUPABASE_ANON_KEY)
