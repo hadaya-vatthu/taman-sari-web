a67522018306d93d6340723ac1377ad5e56bba18
Jordi Yaputra <jordiyaputra@gmail.com>
2022-11-30 14:06:50 +0700

    refactor: change welcome page layout

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,31 +3,27 @@
 	import { navbarTitle } from '$lib/stores';
 	import { sessionSt } from '$lib/stores';
 	import Button from '@smui/button';
+	import Hero from './_Hero.svelte';
 
 	navbarTitle.set('Taman Sari');
 </script>
 
-<div id="hero">
-	<h1>Welcome to Taman Sari</h1>
-</div>
+<Hero />
 
 <div class="container">
-	<Button href="/daily-info" on:mouseover={() => prefetch('/daily-info')}>Daily Information</Button>
+	<Button variant="outlined" href="/daily-info" on:mouseover={() => prefetch('/daily-info')}
+		>Daily Information</Button
+	>
 	{#if $sessionSt}
-		<Button href="/attendance" on:mouseover={() => prefetch('/attendance')}
+		<Button variant="outlined" href="/attendance" on:mouseover={() => prefetch('/attendance')}
 			>Attendance Management</Button
 		>
 	{/if}
 </div>
 
 <style>
-	#hero {
-		padding: 16px;
-	}
 	.container {
 		display: flex;
 		flex-direction: column;
-		align-items: flex-start;
-		padding: 16px;
 	}
 </style>
---
+++ src/routes/_Hero.svelte
@@ -0,0 +1,26 @@
+<div id="hero">
+	<div class="mdc-typography--headline6">Welcome to,</div>
+
+	<div class='center'>
+		<div class="mdc-typography--headline2"><strong>PATVDH - TS</strong></div>
+		<div class="mdc-typography--body1">Pa Auk Tawya Vipassana Dhura Hermitage</div>
+		<div class="mdc-typography--body1">Taman Sari</div>
+	</div>
+</div>
+
+<style type="text/scss">
+	@use '_Hero.scss';
+
+	#hero {
+		display: flex;
+		flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 50vh;
+	}
+  .center {
+    display: flex;
+		flex-direction: column;
+    align-items: center;
+  }
+</style>
